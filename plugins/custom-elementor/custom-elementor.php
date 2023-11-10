ac7f069ba158ef5a08a299c0d43a5f5cb3aa7e8e
RaikkoP <raikko.prants@gmail.com>
2023-11-11 01:14:55 +0200

    first commit

---
+++ plugins/custom-elementor/custom-elementor.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: Custom Elementor
+ * Description: Enda loodud Elementor pluginad, et veebilehe uuendamine ja hoidmine oleks mugavam
+ * Version: 1.0.0
+ * Author: RaikkoP
+ * Text Domain: custom-elementor
+ * 
+ * Elementor tested up to 3.9.0
+ * Elementor Pro tested up to 3.9.0
+ */
