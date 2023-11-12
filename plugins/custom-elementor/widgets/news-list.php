a37ea94c50a76f3dceb9a271643d8ac2c15eb27f
RaikkoP <raikko.prants@gmail.com>
2023-11-12 04:48:40 +0200

    Commented process a bit better

--- plugins/custom-elementor/widgets/news-list.php
+++ plugins/custom-elementor/widgets/news-list.php
@@ -10,9 +10,13 @@ if (!defined("ABSPATH")){
 //Selleks, et funktsiooni toole saada loome funktsiooni nimega callApi mis votab vastu vajaliku informatsiooni
 //Funktsioon on kirjutatud dokumentatsiooni jargi
 function callAPI($url){
+    //Alustame uut curl seanssi
     $curl = curl_init();
+    //See maaratleb ulekande viisi, sel juhul me tahame serverilt infot edasi kanda stringina
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    //Sellega maarame ulekande URL-i
     curl_setopt($curl, CURLOPT_URL, $url);
+    //Saadame HTTP paringu ja salvestame tulemused
     $result = curl_exec($curl);
     if(!$result){die("Connection Failure");}
     curl_close($curl);
@@ -20,6 +24,7 @@ function callAPI($url){
 }
 //Votame kasutusele nuud API kaudu saadud informatsiooni
 $get_data = callAPI('https://puurmani.edu.ee/kool/wp-json/wp/v2/posts?per_page=5');
+//Kuna vastus on JSON data siis on vaja see teha PHP loetavaks
 $response = json_decode($get_data, true);
 
 
